f0ea2064960ff893b7f4173964c0f2c804105481
lee4339 <skyho4339@naver.com>
2022-05-20 12:54:43 +0900

    스프링부트 수업9(너무 많은걸 해서 어지워....요오)

--- study/src/main/java/com/springboot/study/config/SecurityConfig.java
+++ study/src/main/java/com/springboot/study/config/SecurityConfig.java
@@ -1,19 +1,73 @@
 package com.springboot.study.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @EnableWebSecurity
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter{
 	
+	@Bean
+	public BCryptPasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http.csrf().disable();
 		http.authorizeRequests()
+			.antMatchers("/api/board/**", "/", "/board/list")
+			.authenticated() //인증을 거쳐라.
+			.antMatchers("/api/v1/user/**")
+			.access("hasRole('ROLE_USER') or hasRole('ROLE_MANAGER') or hasRole('ROLE_ADMIN')")
+			.antMatchers("/api/v1/manager/**")
+			.access("hasRole('ROLE_MANAGER') or hasRole('ROLE_ADMIN')")
+			.antMatchers("/api/v1/admin/**")
+			.access("hasRole('ROLE_ADMIN')")
 			.anyRequest()
-			.permitAll();
+			.permitAll() //권한이 필요없다.
+			.and()
+			.formLogin()
+			.loginPage("/auth/signin") //로그인 페이지 get 요청(view)
+			.loginProcessingUrl("/auth/signin") //로그인 post요청(PrincipalDetailsService -> loadUserByUsername() 호출)
+			.defaultSuccessUrl("/");
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
 	}
 }
